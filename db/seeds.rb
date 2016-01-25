9652b00c1843c0f34248beefa6d858499f05df98
k.sato <ksato@netprotections.co.jp>
2016-01-25 16:30:33 +0900

    [add] seedsファイル修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,23 +1,16 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
-
-
 # シード投入用ファイル
-# target_file_names = %w(  seed_for_settlement_api_integration_test )
-# target_file_names = %w( seed_for_member_portal_integration_test )
-# target_file_names = %w( seed_for_member_auth_module_integration_test )
+# target_file_names = %w(  actions )
+# target_file_names = %w( answer_histories )
+# target_file_names = %w( matches )
+# target_file_names = %w( questions )
+# target_file_names = %w( shops )
+# target_file_names = %w( users )
 # property_file_names = %w( seed_for_business_message_properties seed_for_business_rule_properties seed_for_system_properties )
 # member-portal test
 target_file_names = %w( actions )
 # development, production それぞれで対象のシード用ファイルを準備、環境に合わせて実行
 target_file_names.each do |target_file|
-  path = Rails.root.join("db/seeds", Rails.env, target_file + ".rb")
-  p path
+  path = Rails.root.join("db/seeds/development/", Rails.env, target_file + ".rb")
   if File.exist?(path)
     puts "Loading #{Rails.env}:#{target_file}..."
     require path
--- db/seeds/development/actions.rb
+++ db/seeds/development/actions.rb
@@ -1,36 +1,36 @@
 Actions.create(
     :id => 6,
     :action_kind => 1,
-    :created_at => ,
-    :updated_at => ,
+    :created_at => nil,
+    :updated_at => nil,
     :shop_id => 2,
-    :user_id => ,
+    :user_id => nil,
 )
 
 Actions.create(
     :id => 7,
     :action_kind => 1,
-    :created_at => ,
-    :updated_at => ,
+    :created_at => nil,
+    :updated_at => nil,
     :shop_id => 2,
-    :user_id => ,
+    :user_id => nil,
 )
 
 Actions.create(
     :id => 8,
     :action_kind => 1,
-    :created_at => ,
-    :updated_at => ,
+    :created_at => nil,
+    :updated_at => nil,
     :shop_id => 2,
-    :user_id => ,
+    :user_id => nil,
 )
 
 Actions.create(
     :id => 9,
     :action_kind => 1,
-    :created_at => ,
-    :updated_at => ,
+    :created_at => nil,
+    :updated_at => nil,
     :shop_id => 2,
-    :user_id => ,
+    :user_id => nil,
 )
 
