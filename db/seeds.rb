1291c69cc37a530714ed494ea3d2507e3684e8cc
y.aizawa <yaizawa@netprotections.co.jp>
2016-01-25 16:59:41 +0900

    Merge branch 'master' of https://np-develop.backlog.jp/git/NP_LUNCH/chikame_no_recome
    # Conflicts:
    #	db/seeds/development/actions.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,13 @@
-p "seed start"
 # シード投入用ファイル
 target_file_names = %w( matches questions shops users )
 # development, production それぞれで対象のシード用ファイルを準備、環境に合わせて実行
 target_file_names.each do |target_file|
-  path = Rails.root.join("db/seeds/development/", Rails.env, target_file + ".rb")
+  path = Rails.root.join("db/seeds/", Rails.env, target_file + ".rb")
   p path
+  File.exist?(path)
   if File.exist?(path)
     puts "Loading #{Rails.env}:#{target_file}..."
     require path
   end
 end
-p "seed end"
+
