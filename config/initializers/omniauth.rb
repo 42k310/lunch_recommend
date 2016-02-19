09881e8aea807f0d748cee393704a39c032b2782
y.aizawa <yaizawa@netprotections.co.jp>
2016-02-19 17:09:52 +0900

    [fix]固定URLをproduction対応に変更

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,5 +1,5 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, Settings.google_api.client_id, Settings.google_api.client_secret
+  provider :google_oauth2, Settings.google_api.client_id, Settings.google_api.client_secret, :skip_jwt => true
 end
 OmniAuth.config.on_failure = Proc.new { |env|
   OmniAuth::FailureEndpoint.new(env).redirect_to_failure
