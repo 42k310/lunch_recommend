80255beab8e898c58788a04bd32306bdce4e06a2
k.sato <ksato@netprotections.co.jp>
2016-03-18 17:59:20 +0900

    [mod][add]エラーページを動的に扱えるように修正、追加（再）

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,9 +1,23 @@
-# class ErrorsController < ApplicationController
 class ErrorsController < ActionController::Base
-  def error_404
-    render action: "errors/error_404", status: 404
+  layout 'application'
+
+  rescue_from ActiveRecord::RecordNotFound, with: :error_404
+  rescue_from ActionController::RoutingError, with: :error_404
+  rescue_from StandardError, with: :error_500
+
+  def error_404(exception = nil)
+    if exception
+      logger.info "Rendering 404 with exception: #{exception.message}"
+    end
+    render template: "errors/error_404", status: 404, layout: 'application'
   end
-  def error_500
-    render action: "errors/error_500", status: 500
+
+  def error_500(exception = nil)
+    if exception
+      logger.info "Rendering 500 with exception: #{exception.message}"
+    end
+    render template: "errors/error_500", status: 500, layout: 'application'
   end
+
+  def show; raise env["action_dispatch.exception"]; end
 end
